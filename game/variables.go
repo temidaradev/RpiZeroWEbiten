e13cdfc244c2398c526325f105beaf838764726c
Barış <duurkak@yandex.com>
2024-11-17 22:55:31 +0300

    düzenleme

--- game/variables.go
+++ game/variables.go
@@ -1,14 +1,11 @@
 package game
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/setanarut/kamera/v2"
 )
 
 var (
-	w, h                                = 635., 475.
-	camSpeed, zoomSpeedFactor, rotSpeed = 7.0, 1.02, 0.02
-	targetX, targetY                    = w / 2, h / 2
-	cam                                 = kamera.NewCamera(targetX, targetY, w, h)
-	dio                                 = &ebiten.DrawImageOptions{}
+	w, h             = 635., 475.
+	targetX, targetY = w / 2, h / 2
+	cam              = kamera.NewCamera(targetX, targetY, w, h)
 )
