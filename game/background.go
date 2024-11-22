95caddf0e96b79394984708f9c23695110d99f1a
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-11-22 19:14:10 +0300

    Update background.go

--- game/background.go
+++ game/background.go
@@ -10,7 +10,7 @@ func MakeBackground(screen *ebiten.Image) {
 		for j := 0; j < 100; j++ {
 			dio := &ebiten.DrawImageOptions{}
 			dio.GeoM.Translate(1800-float64(i*50), 1700-float64(j*50))
-			cam.Draw(assets.Tiles[1], dio, screen)
+			screen.DrawImage(assets.Tiles[1], dio)
 		}
 	}
 }
